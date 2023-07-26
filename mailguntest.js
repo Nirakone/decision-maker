2f974404b3fd4513e591a5f02e6ca6a1f4b54914
sonder03 <ravneet.virk@gmail.com>
2023-07-26 17:20:17 -0400

    Save mailgun implementation

---
+++ mailguntest.js
@@ -0,0 +1,27 @@
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+const mailgun = new Mailgun(formData);
+
+function sendemail() {
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY,
+    url: process.env.MAILGUN_API_URL,
+  });
+
+console.log(process.env.MAILGUN_API_KEY);
+console.log(process.env.MAILGUN_API_URL);
+
+  mg.messages
+    .create(process.env.MAILGUN_DOMAIN, {
+      from: process.env.MAILGUN_SENDER_EMAIL,
+      to: ["ravneet.virk@gmail.com"],
+      subject: "Hello",
+      text: "Testing some Mailgun awesomeness!",
+      html: "<h1>Testing some Mailgun awesomeness!</h1>",
+    })
+    .then((msg) => console.log(msg)) // logs response data
+    .catch((err) => console.log(err)); // logs any error
+}
+
+module.exports = sendemail;
