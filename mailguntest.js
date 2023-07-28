8931d05f5986c02b985e6214b67476f3ffa48eca
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-07-28 19:23:22 -0400

    commented code for future flexibility

--- mailguntest.js
+++ mailguntest.js
@@ -1,4 +1,4 @@
-// const formData = require("form-data");
+const formData = require("form-data");
 const Mailgun = require("mailgun.js");
 const mailgun = new Mailgun(formData);
 
@@ -15,7 +15,7 @@ console.log(process.env.MAILGUN_API_URL);
   mg.messages
     .create(process.env.MAILGUN_DOMAIN, {
       from: process.env.MAILGUN_SENDER_EMAIL,
-      to: ["ravneet.virk@gmail.com, "],
+      to: ["ravneet.virk@gmail.com"],
       subject: "Hello",
       text: "Testing some Mailgun awesomeness!",
       html: "<h1>Testing some Mailgun awesomeness!</h1>",
@@ -25,3 +25,35 @@ console.log(process.env.MAILGUN_API_URL);
 }
 
 module.exports = sendemail;
+
+//test code below to modify to send to anyone in future
+
+// function sendEmail(recipientEmail, subject, textContent, htmlContent) {
+//   const mg = mailgun.client({
+//     username: "api",
+//     key: process.env.MAILGUN_API_KEY,
+//     url: process.env.MAILGUN_API_URL,
+//   });
+
+//   console.log(process.env.MAILGUN_API_KEY);
+//   console.log(process.env.MAILGUN_API_URL);
+
+//   mg.messages
+//     .create(process.env.MAILGUN_DOMAIN, {
+//       from: process.env.MAILGUN_SENDER_EMAIL,
+//       to: [recipientEmail],
+//       subject: subject,
+//       text: textContent,
+//       html: htmlContent,
+//     })
+//     .then((msg) => console.log(msg)) // logs response data
+//     .catch((err) => console.log(err)); // logs any error
+// }
+
+// module.exports = sendEmail;
+
+
+
+
+
+
